8e2b9851075efce14b7b7209d7deb96c00c1bff7
SMFahimShahrear <s.m.shahrear.fahim@gamil.com>
2022-10-27 22:00:34 +0600

    js added

---
+++ script.js
@@ -0,0 +1,14 @@
+let password_show_checkbox = document.querySelector("#checkbox-show-pass");
+let password_input = document.querySelector(".password");
+
+
+
+function password_visibility(e){
+    console.log("function_called");
+    if(e.checked == true){
+// else if(password_input.checked == false){
+    // e.type = "text";
+    console.log(password_input.type)
+    console.log("pass");
+}
+}
